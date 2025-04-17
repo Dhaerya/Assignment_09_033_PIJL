139faea9a48e6f9804378bdb939c82dccba200e0
Dhaerya <dhaeryamore@gmail.com>
2025-04-17 20:54:50 +0530

    Created Main.java with main method and app launcher

---
+++ Main.java
@@ -0,0 +1,10 @@
+// Name: Dhaerya More
+// PRN: 23070126033
+// Batch: A2
+
+public class Main {
+    public static void main(String[] args) {
+        StudentApp app = new StudentApp();
+        app.menu();
+    }
+}
